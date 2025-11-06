6d87711f8c7072d6827d704adfe95205ff9c4443
nurainisiti662-lang <nurainisiti662@gmail.com>
2025-11-06 16:43:04 +0700

    Update testcase with pom

---
+++ cypress/e2e/latihan_pom.cy.js
@@ -0,0 +1,58 @@
+import loginPage from "../support/loginFeature";
+import loginData from "../fixtures/loginData.json"
+
+describe('OrangeHRM Login Feature', () => {
+  it('TC_LOGIN_001 - Login with credentials valid', () => {
+    //cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',  {timeout: 10000});
+    //cy.get('input[placeholder="Username"]').type('Admin'), {timeout: 10000};
+    //cy.get('input[placeholder="Password"]').type('admin123'), {timeout: 10000};
+    //cy.get('button[type="submit"]').click(), {timeout: 10000};
+    //cy.contains('Dashboard').should('be.visible'), {timeout: 10000};
+    
+    loginPage.visit(), {timeout: 10000};
+    loginPage.inputUsername(loginData.validUsername), {timeout: 10000};
+    loginPage.inputPassword(loginData.validPassword), {timeout: 10000};
+    loginPage.clickLoginButton(), {timeout: 10000};
+    loginPage.assertionLogin(), {timeout: 10000};
+  });
+
+  it('TC_LOGIN_002 - Login with invalid password ', () => {
+    loginPage.visit(), {timeout: 10000};
+    loginPage.inputUsername(loginData.validUsername), {timeout: 10000};
+    loginPage.inputPassword(loginData.invalidPassword), {timeout: 10000};
+    loginPage.clickLoginButton(), {timeout: 10000};
+  });
+
+
+  it('TC_LOGIN_003 - Login with invalid username ', () => {
+    loginPage.visit(), {timeout: 10000};
+    loginPage.inputUsername(loginData.invalidUsername), {timeout: 10000};
+    loginPage.inputPassword(loginData.validPassword), {timeout: 10000};
+    loginPage.clickLoginButton(), {timeout: 10000};
+  });
+
+
+  it('TC_LOGIN_004 - Login without input username and password', () => {
+    loginPage.visit(), {timeout: 10000};
+    loginPage.clickLoginButton(), {timeout: 10000};
+  });
+
+   it('TC_LOGIN_005 - Refresh after failed login', () => {
+    loginPage.visit(), {timeout: 10000};
+    loginPage.clickLoginButton(), {timeout: 10000};
+    loginPage.reload(), {timeout: 10000};
+  });
+
+  it('TC_LOGIN_006 - Verify the Login button is enabled only when both fields are filled', () => {
+    loginPage.visit(), {timeout: 10000};
+    loginPage.inputPassword(loginData.validPassword), {timeout: 10000};
+    loginPage.clickLoginButton(), {timeout: 10000};
+  });
+
+ it('TC_LOGIN_007 - Verify login with space before username', () => {
+    loginPage.visit(), {timeout: 10000};
+    loginPage.inputUsername(loginData.spaceBeforeUsername), {timeout: 10000};
+    loginPage.inputPassword(loginData.validPassword), {timeout: 10000};
+    loginPage.clickLoginButton(), {timeout: 10000};
+  });
+});
---
+++ cypress/support/loginFeature.js
@@ -0,0 +1,22 @@
+class loginPage{
+    visit(){ 
+      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    }
+    inputUsername(username){
+      cy.get('input[placeholder="Username"]').type('Admin')
+    }
+    inputPassword(password){
+      cy.get('input[placeholder="Password"]').type('admin123')
+    }
+    clickLoginButton(){
+      cy.get('button[type="submit"]').click()
+    }
+    assertionLogin(){
+      cy.contains('Dashboard').should('be.visible')
+    }
+    reload(){
+      cy.reload()
+    }
+}   
+
+export default new loginPage()
